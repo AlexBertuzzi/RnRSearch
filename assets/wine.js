58b2b47bd69be0f40cc02789c7d7596b2924fc48
Marshall Bertschy <basemarsh@gmail.com>
2020-11-19 22:41:33 -0600

    trying to capitolize first letters of words

--- assets/wine.js
+++ assets/wine.js
@@ -1,5 +1,5 @@
-$(".uk-form-icon").on("click", function (event) {
-var food =  $(".uk-form-width-large").val()
+$("#searchBtn").on("click", function (event) {
+var food =  $("#userInput").val()
 event.preventDefault()
 console.log(food)
 const settings = {
@@ -15,5 +15,48 @@ const settings = {
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
+	$("#exp").text(response.pairingText)
+	for (var i = 0; i< response.pairedWines.length; i++){
+	var pairedWines = response.pairedWines
+	var pairedWinesCapitolize = splitStrings(pairedWines)
+	console.log(pairedWinesCapitolize)
+	// $("#wineName-"+[i]).text();
+	$("#wineName-"+[i]).text(pairedWinesCapitolize[i])
+	}	
 })
 })
+
+var array =["merlot",
+"cabernet sauvignon","pinot noir"]
+
+splitStrings(array)
+
+
+
+function splitStrings(arr){
+	var resultArray = []
+	for (var i = 0; i < arr.length; i++) {
+		var arrayItem = arr[i]
+		var itemStrings = arrayItem.split(" ")
+		console.log(itemStrings)
+		var titleCaseString = toTitleCase2(itemStrings)
+		resultArray.push(titleCaseString)
+	} 
+	console.log(resultArray)
+	return resultArray
+}
+
+
+
+function toTitleCase2(strArr){
+	var result = ""
+	$.each(strArr, function (key, value) {
+		var titleCase = value.replace(/\w\S*/g, function (txt) {
+			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		});
+		result += (titleCase+" ")
+		console.log(result)
+	})
+	return result
+
+}
