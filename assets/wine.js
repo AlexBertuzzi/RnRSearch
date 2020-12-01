8a5f357faa340a0571617786bfdde54a2b65a958
Marshall Bertschy <basemarsh@gmail.com>
2020-11-30 23:15:06 -0600

    working on adding buttons for shop

--- assets/wine.js
+++ assets/wine.js
@@ -1,107 +1,120 @@
-var testWD = ""
-$("#searchBtn").on("click", function (event) {
-	$("#wines").empty()
-	var food = $("#userInput").val()
-	event.preventDefault()
-	console.log(food)
+$(document).ready(function () {
+	$("#searchBtn").on("click", function (event) {
+		$("#wines").empty()
+		var food = $("#userInput").val()
+		event.preventDefault()
+		console.log(food)
+		const settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/pairing?food=" + food,
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-key": "578be9e848msh571074644e04840p1eca5bjsnc902c4bed763",
+				"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+			}
+		};
+
+		$.ajax(settings).done(function (response) {
+			console.log(response);
+			var pairedWines,
+				newDT,
+				newDD,
+				shopBTN;
+			$("#exp").text(response.pairingText)
+			var numberOfWines = response.pairedWines.length
+			for (var i = 0; i < numberOfWines; i++) {
+				pairedWines = response.pairedWines
+				newDT = "<dt class='capitolize' id=" + "wineName-" + [i] + ">PlaceHolder Title</dt>"
+				newDD = "<dd id=" + "wineDescription-" + [i] + "></dd>"
+				shopBTN = "<button id='shop-" + [i] + "'>Recommendations</button>"
+				$("#wines").append(newDT)
+				$("#wineName-" + [i]).after(newDD)
+				$("#wineName-" + [i]).after(shopBTN)
+				$("#wineName-" + [i]).text(pairedWines[i])
+				$("#shop-" + [i]).addClass("uk-align-right")
+				console.log(pairedWines[i])
+			}
+			for (var i = 0; i < numberOfWines; i++) {
+				console.log(numberOfWines)
+				getDesc(i, pairedWines)
+			};
+
+		});
+	})
+
+
+	function splitStrings(arr) {
+		var resultArray = []
+		for (var i = 0; i < arr.length; i++) {
+			var arrayItem = arr[i]
+			var itemStrings = arrayItem.split(" ")
+			var titleCaseString = toTitleCase(itemStrings)
+			resultArray.push(titleCaseString)
+		}
+		return resultArray
+	}
+
+	function getDesc(i, pairedWines) {
+		const settings2 = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/description?wine=" + pairedWines[i] + "",
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-key": "578be9e848msh571074644e04840p1eca5bjsnc902c4bed763",
+				"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+			}
+		};
+
+		$.ajax(settings2).done(function (response2) {
+			console.log(response2);
+			console.log(response2.wineDescription)
+			console.log(i)
+			$("#wineDescription-" + [i]).text(response2.wineDescription)
+		}
+		)
+	}
+	function toTitleCase(strArr) {
+		var result = ""
+		$.each(strArr, function (key, value) {
+			var titleCase = value.replace(/\w\S*/g, function (txt) {
+				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+			});
+			result += (titleCase + " ")
+		})
+		return result
+
+	}
+function wineRec() {
 	const settings = {
 		"async": true,
 		"crossDomain": true,
-		"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/pairing?food=" + food,
+		"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/recommendation?wine=" + wine + "&maxPrice=200&minRating=0.7&number=4",
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-key": "578be9e848msh571074644e04840p1eca5bjsnc902c4bed763",
 			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
 		}
 	};
-
 	$.ajax(settings).done(function (response) {
 		console.log(response);
-		var pairedWines,
-			newDT,
-			newDD,
-			shopBTN;
-		$("#exp").text(response.pairingText)
-		var numberOfWines = response.pairedWines.length
-		for (var i = 0; i < numberOfWines; i++) {
-			pairedWines = response.pairedWines
-			newDT = "<dt class='capitolize' id=" + "wineName-" + [i] + ">PlaceHolder Title</dt>"
-			newDD = "<dd id=" + "wineDescription-" + [i] + "></dd>"
-			shopBTN = "<button class=" + "uk-align-right" + " shop"+">Recommendations</button>"
-			$("#wines").append(newDT)
-			$("#wineName-" + [i]).after(newDD)
-			$("#wineName-" + [i]).after(shopBTN)
-			$("#wineName-" + [i]).text(pairedWines[i])
-			$(".shop-"+[i]).attr('id', pairedWines[i])
-			console.log(pairedWines[i])
-		}
-		for (var i = 0; i < numberOfWines; i++) {
-			console.log(numberOfWines)
-			getDesc(i, pairedWines)
-		};
-
 	});
-})
-
-
-function splitStrings(arr) {
-	var resultArray = []
-	for (var i = 0; i < arr.length; i++) {
-		var arrayItem = arr[i]
-		var itemStrings = arrayItem.split(" ")
-		var titleCaseString = toTitleCase(itemStrings)
-		resultArray.push(titleCaseString)
-	}
-	return resultArray
 }
 
-function getDesc(i, pairedWines) {
-	const settings2 = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/description?wine=" + pairedWines[i] + "",
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-key": "578be9e848msh571074644e04840p1eca5bjsnc902c4bed763",
-			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-		}
-	};
+$('#shop-0').on('click', function () {
+	var wine = (this.id)
+	console.log(this.id)
+	wineRec(wine)
+});
 
-	$.ajax(settings2).done(function (response2) {
-		console.log(response2);
-		console.log(response2.wineDescription)
-		console.log(i)
-		$("#wineDescription-" + [i]).text(response2.wineDescription)
-	}
-	)
-}
-function toTitleCase(strArr) {
-	var result = ""
-	$.each(strArr, function (key, value) {
-		var titleCase = value.replace(/\w\S*/g, function (txt) {
-			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		});
-		result += (titleCase + " ")
-	})
-	return result
+$('#shop-1').on('click', function () {
+	var wine = (this.id)
+	wineRec(wine)
+});
 
-}
-
-$(".shop").on("click", function (event) {
-	var wine = $(this.id)
-	console.log(wine)
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/recommendation?wine="+ +"&maxPrice=50&minRating=0.7&number=4",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "578be9e848msh571074644e04840p1eca5bjsnc902c4bed763",
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
+$('#shop-2').on('click', function () {
+	var wine = (this.id)
+	wineRec(wine)
 });
 })
