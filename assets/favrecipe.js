8e8f8212faaf687379d9c1ce3f3c652010c7bcaf
Melody Monarch <oliva.sam.so@gmail.com>
2020-12-01 20:49:09 -0600

    favorites page finished and hardcode removed

--- assets/favrecipe.js
+++ assets/favrecipe.js
@@ -1,30 +1,36 @@
-//favorites tab area
 
-console.log("hi")
+var favoriteRecipe = JSON.parse(localStorage.getItem("favRecipe")) || []
+for (i = 0; i < favoriteRecipe.length; i++) {
 
-function lookupFavFood(food) {
+    var recipeFav = favoriteRecipe[i]
+   // console.log(recipeFav)
+   $(".favArea").append(`<dt id="${i}-favoriteName"></dt><dd id="${i}-favoriteDescription"></dd>`)
+    lookupHisFood(recipeFav, i)
+}
+
+function lookupHisFood(food, index) {
     var queryURL = `https://api.edamam.com/search?q=${food}&app_id=2bf575c8&app_key=5b2e4bc88630bc1e0a7628be37de02fd`
     $.ajax({
         method: "GET",
         url: queryURL
     }).then(function (apiData) {
-        //console.log(apiData)
-        for (var i = 0; i < 5; i++) {
-            var foodPic = apiData.hits[i].recipe.image
-            var recipeTitle = apiData.hits[i].recipe.label
-            var ingredients = apiData.hits[i].recipe.ingredientLines
-            var recipeLink = apiData.hits[i].recipe.url
-            var servings = apiData.hits[i].recipe.yield
-            var caloriesPer = ((apiData.hits[i].recipe.calories) / servings).toFixed(0)
-            $("#" + i + "-recipeName").html(`<p> <a href="${recipeLink}" target="_blank">
+        console.log(apiData)
+     
+            var foodPic = apiData.hits[0].recipe.image
+            var recipeTitle = apiData.hits[0].recipe.label
+            var ingredients = apiData.hits[0].recipe.ingredientLines
+            var recipeLink = apiData.hits[0].recipe.url
+            var servings = apiData.hits[0].recipe.yield
+            var caloriesPer = ((apiData.hits[0].recipe.calories) / servings).toFixed(0)
+
+            $("#" + index + "-favoriteName").html(`<p> <a href="${recipeLink}" target="_blank">
                 ${recipeTitle} | Servings: ${servings} | Calories: ${caloriesPer} per serving </a></p>`)
-            $("#" + i + "-heartBtn").attr("data-recipe", recipeTitle)
-            $("#" + i + "-recipeDescription").html("")
+
+            $("#" + index + "-favoriteDescription").html("")
             for (var j = 0; j < ingredients.length; j++) {
-                $("#" + i + "-recipeDescription").append(`<img id="${i}-recipeImage"><li> ${ingredients[j]} </li>`)
-                $("#" + i + "-recipeImage").attr("src", foodPic)
+                $("#" + index + "-favoriteDescription").append(`<img id="${index}-favoriteImage"><li> ${ingredients[j]} </li>`)
+                $("#" + index + "-favoriteImage").attr("src", foodPic)
             }
-        }
 
     })
 }
--- assets/recipe.js
+++ assets/recipe.js
@@ -1,3 +1,6 @@
+var defaultRecipe = "turkey"
+lookupUserFood(defaultRecipe)
+
 $("#searchBtn").on("click", function (event) {
     event.preventDefault()
     var userFood = $("#userInput").val()
@@ -20,33 +23,37 @@ function lookupUserFood(food) {
             var servings = apiData.hits[i].recipe.yield
             var caloriesPer = ((apiData.hits[i].recipe.calories) / servings).toFixed(0)
             $("#" + i + "-recipeName").html(`<p> <a href="${recipeLink}" target="_blank">
-                ${recipeTitle} | Servings: ${servings} | Calories: ${caloriesPer} per serving </a></p>`)
+                ${recipeTitle} | Servings: ${servings} | Calories: ${caloriesPer} per serving </a></p> 
+                <button class="uk-align-right heartBtn" uk-icon="heart" id= "${i}-heartBtn"></button>`)
             $("#" + i + "-heartBtn").attr("data-recipe", recipeTitle)
             $("#" + i + "-recipeDescription").html("")
             for (var j = 0; j < ingredients.length; j++) {
                 $("#" + i + "-recipeDescription").append(`<img id="${i}-recipeImage"><li> ${ingredients[j]} </li>`)
                 $("#" + i + "-recipeImage").attr("src", foodPic)
+
             }
         }
-
+        
+        $(".heartBtn").on("click", function () {
+            //console.log("hi")
+        
+            var favoriteRecipe = JSON.parse(localStorage.getItem("favRecipe")) || []
+            var currentRecipe = $(this).attr("data-recipe")
+            // console.log(currentRecipe)
+            var checkRepeat = favoriteRecipe.indexOf(currentRecipe)
+           // console.log(checkRepeat)
+            if (currentRecipe !== undefined && (checkRepeat === -1 || !favoriteRecipe)) {
+                favoriteRecipe.push(currentRecipe)
+                localStorage.setItem("favRecipe", JSON.stringify(favoriteRecipe))
+                this.classList.add("colorHeart")
+            }
+        
+            console.log(favoriteRecipe)
+        
+        })
     })
 }
-$(".heartBtn").on("click", function () {
 
-    var favoriteRecipe = JSON.parse(localStorage.getItem("favRecipe")) || []
-    var currentRecipe = $(this).attr("data-recipe")
-    // console.log(currentRecipe)
-    var checkRepeat = favoriteRecipe.indexOf(currentRecipe)
-   // console.log(checkRepeat)
-    if (currentRecipe !== undefined && (checkRepeat === -1 || !favoriteRecipe)) {
-        favoriteRecipe.push(currentRecipe)
-        localStorage.setItem("favRecipe", JSON.stringify(favoriteRecipe))
-        this.classList.add("colorHeart")
-    }
-
-    // console.log(favoriteRecipe)
-
-})
 
 
 
