8fb2352b7772a5fba3542312bf339fdd70bf098f
Melody Monarch <oliva.sam.so@gmail.com>
2020-12-01 21:07:26 -0600

    cleaned up code and added comments to js

--- assets/favrecipe.js
+++ assets/favrecipe.js
@@ -1,10 +1,14 @@
+// JS for favorites tab
 
+// Get the recipe titles of each favorited recipe in our local storage
 var favoriteRecipe = JSON.parse(localStorage.getItem("favRecipe")) || []
 for (i = 0; i < favoriteRecipe.length; i++) {
-
     var recipeFav = favoriteRecipe[i]
-   // console.log(recipeFav)
-   $(".favArea").append(`<dt id="${i}-favoriteName"></dt><dd id="${i}-favoriteDescription"></dd>`)
+
+    // dynamically adding our elements for each favorite
+    $(".favArea").append(`<dt id="${i}-favoriteName"></dt><dd id="${i}-favoriteDescription"></dd>`)
+
+    // for each index in the local storage array, we are going to call the function
     lookupHisFood(recipeFav, i)
 }
 
@@ -14,23 +18,23 @@ function lookupHisFood(food, index) {
         method: "GET",
         url: queryURL
     }).then(function (apiData) {
-        console.log(apiData)
-     
-            var foodPic = apiData.hits[0].recipe.image
-            var recipeTitle = apiData.hits[0].recipe.label
-            var ingredients = apiData.hits[0].recipe.ingredientLines
-            var recipeLink = apiData.hits[0].recipe.url
-            var servings = apiData.hits[0].recipe.yield
-            var caloriesPer = ((apiData.hits[0].recipe.calories) / servings).toFixed(0)
 
-            $("#" + index + "-favoriteName").html(`<p> <a href="${recipeLink}" target="_blank">
+        // we want the [0] hit of the ajax call for that food because that is what the user favorites by default
+        var foodPic = apiData.hits[0].recipe.image
+        var recipeTitle = apiData.hits[0].recipe.label
+        var ingredients = apiData.hits[0].recipe.ingredientLines
+        var recipeLink = apiData.hits[0].recipe.url
+        var servings = apiData.hits[0].recipe.yield
+        var caloriesPer = ((apiData.hits[0].recipe.calories) / servings).toFixed(0)
+
+        $("#" + index + "-favoriteName").html(`<p> <a href="${recipeLink}" target="_blank">
                 ${recipeTitle} | Servings: ${servings} | Calories: ${caloriesPer} per serving </a></p>`)
 
-            $("#" + index + "-favoriteDescription").html("")
-            for (var j = 0; j < ingredients.length; j++) {
-                $("#" + index + "-favoriteDescription").append(`<img id="${index}-favoriteImage"><li> ${ingredients[j]} </li>`)
-                $("#" + index + "-favoriteImage").attr("src", foodPic)
-            }
+        $("#" + index + "-favoriteDescription").html("")
+        for (var j = 0; j < ingredients.length; j++) {
+            $("#" + index + "-favoriteDescription").append(`<img id="${index}-favoriteImage"><li> ${ingredients[j]} </li>`)
+            $("#" + index + "-favoriteImage").attr("src", foodPic)
+        }
 
     })
 }
--- assets/recipe.js
+++ assets/recipe.js
@@ -1,6 +1,11 @@
+// JS for Home Page
+
+
+//  Upon landing page in Home, automatic recipes will generate for turkey as an example
 var defaultRecipe = "turkey"
 lookupUserFood(defaultRecipe)
 
+// when user types something into input box and clicks the magnifying glass, will call ajax function
 $("#searchBtn").on("click", function (event) {
     event.preventDefault()
     var userFood = $("#userInput").val()
@@ -8,13 +13,15 @@ $("#searchBtn").on("click", function (event) {
     lookupUserFood(userFood)
 })
 
+// function that will run the ajax call of whatever food user inputted 
 function lookupUserFood(food) {
     var queryURL = `https://api.edamam.com/search?q=${food}&app_id=2bf575c8&app_key=5b2e4bc88630bc1e0a7628be37de02fd`
     $.ajax({
         method: "GET",
         url: queryURL
     }).then(function (apiData) {
-        //console.log(apiData)
+
+        // loop for only 5 recipes because that is the limit on the APi per minute
         for (var i = 0; i < 5; i++) {
             var foodPic = apiData.hits[i].recipe.image
             var recipeTitle = apiData.hits[i].recipe.label
@@ -22,34 +29,37 @@ function lookupUserFood(food) {
             var recipeLink = apiData.hits[i].recipe.url
             var servings = apiData.hits[i].recipe.yield
             var caloriesPer = ((apiData.hits[i].recipe.calories) / servings).toFixed(0)
+
+            // the title with the Recipe Name will have a link to the actual recipe webpage
             $("#" + i + "-recipeName").html(`<p> <a href="${recipeLink}" target="_blank">
                 ${recipeTitle} | Servings: ${servings} | Calories: ${caloriesPer} per serving </a></p> 
                 <button class="uk-align-right heartBtn" uk-icon="heart" id= "${i}-heartBtn"></button>`)
+
+            // data attribute added so we can reference it for our local storage
             $("#" + i + "-heartBtn").attr("data-recipe", recipeTitle)
+
             $("#" + i + "-recipeDescription").html("")
+
+            //loop through the entire ingredients and make a list item for each one
             for (var j = 0; j < ingredients.length; j++) {
                 $("#" + i + "-recipeDescription").append(`<img id="${i}-recipeImage"><li> ${ingredients[j]} </li>`)
                 $("#" + i + "-recipeImage").attr("src", foodPic)
-
             }
         }
-        
+
+        // listener for the favorite button
         $(".heartBtn").on("click", function () {
-            //console.log("hi")
-        
+
             var favoriteRecipe = JSON.parse(localStorage.getItem("favRecipe")) || []
             var currentRecipe = $(this).attr("data-recipe")
-            // console.log(currentRecipe)
+
+            //to avoid repeated favorites or if the user clicks on a heart icon without a recipe 
             var checkRepeat = favoriteRecipe.indexOf(currentRecipe)
-           // console.log(checkRepeat)
             if (currentRecipe !== undefined && (checkRepeat === -1 || !favoriteRecipe)) {
                 favoriteRecipe.push(currentRecipe)
                 localStorage.setItem("favRecipe", JSON.stringify(favoriteRecipe))
                 this.classList.add("colorHeart")
             }
-        
-            console.log(favoriteRecipe)
-        
         })
     })
 }
